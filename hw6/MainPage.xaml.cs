9765534d2e0db944ef13b16efbddad9536180613
Jerry Compton <jerry@Jerrys-MBP.fios-router.home>
2020-03-31 19:52:11 -0700

    Initial Commit

---
+++ hw6/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Xamarin.Forms;
+
+namespace hw6
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        public string WordInput { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            HttpClient Client = new HttpClient();
+
+        }
+    }
+}
