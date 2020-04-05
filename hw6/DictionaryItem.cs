fe96f55cd4690d449fbc2bbeab3efa64b4ca3e34
Jerry Compton <jerry@Jerrys-MBP.fios-router.home>
2020-04-04 17:35:16 -0700

    Dictionary Items are now displaying properly

---
+++ hw6/DictionaryItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace hw6
+{   //OUR CLASS MADE FROM https://app.quicktype.io/#l=cs&r=json2csharp
+
+    public partial class DictionaryItem
+    {
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("definition")]
+        public string Definition { get; set; }
+
+        [JsonProperty("example")]
+        public string Example { get; set; }
+    }
+
+    public partial class DictionaryItem
+    {
+        public static DictionaryItem[] FromJson(string json) => JsonConvert.DeserializeObject<DictionaryItem[]>(json, hw6.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this DictionaryItem[] self) => JsonConvert.SerializeObject(self, hw6.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
--- hw6/MainPage.xaml.cs
+++ hw6/MainPage.xaml.cs
@@ -16,9 +16,13 @@ namespace hw6
     {
 
         public string WordInput { get; set; }
+        //public string type ="";
+        //public string desc ="";
+        //public string example="";
 
         public MainPage()
         {
+            BindingContext = this;
             InitializeComponent();
         }
 
@@ -34,11 +38,67 @@ namespace hw6
 
 
             HttpResponseMessage response = null;
-            response = await Client.SendAsync(request);
+
+            //---
+            
+            try
+            {
+                response = await Client.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                //if it fails, displays an alert to user and ends the function
+                await DisplayAlert("Connection error", "Sorry, something went wrong! Make sure you are connected to the Internet", "OK");
+                return;
+            }
+            //---
+            DictionaryItem[] Data = null;
+
 
             if(response.IsSuccessStatusCode)
             {
-                await DisplayAlert("Success","It worked...","OK");
+                //await DisplayAlert("Success","It worked...","OK");
+
+                string content = await response.Content.ReadAsStringAsync();
+
+                try
+                {
+                    Data = DictionaryItem.FromJson(content);
+                }
+                catch (Exception ex)
+                {
+                    //if json is bad, displays error message and returns the function
+                    await DisplayAlert("Data error", "Something went wrong while retrieving data. Please try again!", "OK");
+                    return;
+                }
+                
+
+
+                for (int i = 0; i < Data.Length; i++)
+                {
+                    string type1 = "";
+                    string desc1 = "";
+                    string example1 = "";
+                    //ifs are needed because some fields may be null
+                    if (Data[i].Type != null)
+                    {
+                        type1 = Data[i].Type;
+                        this.type.Text = type1;
+                        
+                    }
+                    if (Data[i].Definition != null)
+                    {
+                        desc1 = Data[i].Definition;
+                        this.desc.Text = desc1;
+                    }
+                    if (Data[i].Example != null)
+                    {
+                        example1 = Data[i].Example;
+                        this.example.Text = example1;
+                    }
+
+                }
+
             }
         }
     }
